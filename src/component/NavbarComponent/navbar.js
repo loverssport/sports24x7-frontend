06433164f96b9ff06b2b5963b833ffedf9c15f5b
hariom sharma <hariomsharma@harioms-MacBook-Pro.local>
2022-11-22 20:19:16 +0530

    deleted unecessary lines from navbarr

--- src/component/NavbarComponent/navbar.js
+++ src/component/NavbarComponent/navbar.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import { FaFacebookSquare, FaInstagramSquare, FaYoutube, FaYoutubeSquare} from 'react-icons/fa';
 import "./style.css"
 
 const Navbar = () => {
