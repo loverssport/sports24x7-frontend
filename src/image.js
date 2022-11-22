556f4264eb13994db0ec696dc76a7ad4e7dc6ec5
Manas_Punj <Manas_Punj>
2022-11-23 00:17:44 +0530

    Added background-image

---
+++ src/image.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./image.css"
+
+
+function Imagess(){
+    return(
+      <div class="background-image">
+        
+      </div>
+    )
+  }
+  export default Imagess;
