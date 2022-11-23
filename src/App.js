2a9affb5335bfb90413a6c257c17aae17d7099f8
hariom sharma <hariomsharma@harioms-MacBook-Pro.local>
2022-11-23 21:56:11 +0530

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/NavbarComponent/navbar';
 import Imagess from './image';
 import Navbar from './component/Nbar/navbar';
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
